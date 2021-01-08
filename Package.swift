4d2fb171538eba2a6ed81dc939419437869d5572
Wei Zhang <veeamd@gmail.com>
2021-01-08 12:05:59 -0500

    IOS-6516: add spm support

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "FLAnimatedImage",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: "FLAnimatedImage", targets: ["FLAnimatedImage"]),
+    ],
+    targets: [
+        .target(name: "FLAnimatedImage",
+                path: "FLAnimatedImage",
+                exclude: ["Info.plist"],
+                publicHeadersPath: "")
+    ]
+)
+
